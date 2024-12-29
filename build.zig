7c368428033cb9f17dafd17a419ad69f745f42b7
Nico Elbers <nico.b.elbers@gmail.com>
2024-12-29 22:04:38 +0100

    build: add TZif and TZString unit tests

--- build.zig
+++ build.zig
@@ -4,16 +4,35 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_unit_tests = b.addTest(.{
+    const datetime_tests = b.addTest(.{
         .root_source_file = b.path("src/DateTime.zig"),
         .target = target,
         .optimize = optimize,
         // .use_llvm = false,
         // .use_lld = false,
     });
+    const datetime_unit_tests = b.addRunArtifact(datetime_tests);
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const tzstring_tests = b.addTest(.{
+        .root_source_file = b.path("src/TZString.zig"),
+        .target = target,
+        .optimize = optimize,
+        // .use_llvm = false,
+        // .use_lld = false,
+    });
+    const tzstring_unit_tests = b.addRunArtifact(tzstring_tests);
+
+    const tzif_tests = b.addTest(.{
+        .root_source_file = b.path("src/TZif.zig"),
+        .target = target,
+        .optimize = optimize,
+        // .use_llvm = false,
+        // .use_lld = false,
+    });
+    const tzif_unit_tests = b.addRunArtifact(tzif_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&datetime_unit_tests.step);
+    test_step.dependOn(&tzstring_unit_tests.step);
+    test_step.dependOn(&tzif_unit_tests.step);
 }

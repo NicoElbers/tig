e7d548844d07f9aa651d2efb919a08de4bc67f85
Nico Elbers <nico.b.elbers@gmail.com>
2025-01-02 07:08:50 +0100

    TZif: Add isValid function

--- src/TZif.zig
+++ src/TZif.zig
@@ -172,9 +172,17 @@ pub const TZifHeader = struct {
 pub const TZifDataBlock = struct {
     transition_times: []const Transition,
     local_time_type_records: []const LocalTimeRecord,
-    timezone_designation: [:0]const u8,
+    timezone_designation: []const u8,
+
+    /// Leap second records
+    ///
+    /// If these records have an expiration date (as per version 4), this should
+    /// NOT be included in this field. It should be included in `leap_second_expiration`
     leap_second_records: []const LeapSecondRecord,
     leap_second_expiration: ?i64,
+
+    // TODO: Consolidate these two into a single array
+    // this is kinda wasteful and hard to parse
     std_wall_indicators: ?[]const StdWallIndicator,
     ut_local_indicators: ?[]const UtLocalIndicator,
 
@@ -291,20 +299,20 @@ pub const TZifDataBlock = struct {
         };
         errdefer alloc.free(local_time_type_records);
 
-        const timezone_designation: [:0]const u8 = blk: {
+        const timezone_designation: []const u8 = blk: {
             assert(header.charcnt > 0);
 
-            const full_str = try alloc.alloc(u8, header.charcnt);
-            errdefer alloc.free(full_str);
+            const timezone_designation = try alloc.alloc(u8, header.charcnt);
+            errdefer alloc.free(timezone_designation);
 
             // Parse Time zone designations
-            r.readNoEof(full_str) catch return Error.InvalidDataBlock;
+            r.readNoEof(timezone_designation) catch return Error.InvalidDataBlock;
 
             // Must be a null terminated string
-            if (full_str[header.charcnt - 1] != 0)
+            if (timezone_designation[header.charcnt - 1] != 0)
                 return Error.InvalidDataBlock;
 
-            break :blk full_str[0 .. header.charcnt - 1 :0];
+            break :blk timezone_designation;
         };
         errdefer alloc.free(timezone_designation);
 
@@ -497,19 +505,284 @@ pub fn parseTzif(alloc: Allocator, r: AnyReader) !?TZif {
     const data_block = TZifDataBlock.parse(alloc, r, header) catch return null;
     errdefer data_block.deinit(alloc);
 
-    if (header.version == .@"1") return .{
-        .header = header,
-        .data_block = data_block,
-        .footer = null,
-    };
+    if (header.version == .@"1") {
+        const parsed: TZif = .{
+            .header = header,
+            .data_block = data_block,
+            .footer = null,
+        };
+
+        // Final sanity check
+        assert(parsed.isValid());
+
+        return parsed;
+    }
 
     const footer = try TZifFooter.parse(alloc, r);
 
-    return .{
+    const parsed: TZif = .{
         .header = header,
         .data_block = data_block,
         .footer = footer,
     };
+
+    // Final sanity check
+    assert(parsed.isValid());
+    return parsed;
+}
+
+pub fn isValid(tzif: TZif) bool {
+    const header = tzif.header;
+    const data = tzif.data_block;
+
+    // isutcnt
+    // A four-octet unsigned integer specifying the number of UT/local indicators
+    // contained in the data block -- MUST either be zero or equal to "typecnt".
+    if (header.isutcnt != 0 and
+        (header.isutcnt != header.typecnt or
+        data.ut_local_indicators == null or
+        header.isutcnt != data.ut_local_indicators.?.len)) return false;
+
+    // isstdcnt
+    // A four-octet unsigned integer specifying the number of standard/wall indicators
+    // contained in the data block -- MUST either be zero or equal to "typecnt".
+    if (header.isstdcnt != 0 and
+        (header.isstdcnt != header.typecnt or
+        data.std_wall_indicators == null or
+        header.isstdcnt != data.std_wall_indicators.?.len)) return false;
+
+    // leapcnt
+    // A four-octet unsigned integer specifying the number of leap-second records
+    // contained in the data block.
+    const leap_second_record_count = data.leap_second_records.len + @intFromBool(data.leap_second_expiration != null);
+    if (header.leapcnt != leap_second_record_count) return false;
+
+    // timecnt
+    // A four-octet unsigned integer specifying the number of transition times
+    // contained in the data block.
+    if (header.timecnt != data.transition_times.len) return false;
+
+    // typecnt
+    // A four-octet unsigned integer specifying the number of local time type records
+    // contained in the data block -- MUST NOT be zero. (Although local time type records
+    // convey no useful information in files that have non-empty TZ strings but
+    // no transitions, at least one such record is nevertheless required because
+    // many TZif readers reject files that have zero time types.)
+    //
+    // NOTE: zero check omited to be more permissive to bad data,
+    // warning generated while parsing
+    if (header.typecnt != data.local_time_type_records.len) return false;
+
+    // charcnt
+    // A four-octet unsigned integer specifying the total number of octets used by the set of
+    // time zone designations contained in the data block -- MUST NOT be zero. The count includes
+    // the trailing NUL (0x00) octet at the end of the last time zone designation.
+    if (header.charcnt == 0 or header.charcnt != data.timezone_designation.len) return false;
+
+    // transition times
+    // A series of four- or eight-octet UNIX leap time values sorted in strictly
+    // ascending order. Each value is used as a transition time at which the rules
+    // for computing local time may change. The number of time values is specified
+    // by the "timecnt" field in the header.
+    //
+    // Each time value be at least -2^59. (-2^59 is the greatest negated power of
+    // 2 that predates the Big Bang, and avoiding earlier timestamps works around
+    // known TZif reader bugs relating to outlandishly negative timestamps.)
+    //
+    // NOTE: -2^59 check omited, warning generated while parsing
+    //
+    // transition types
+    // A series of one-octet unsigned integers specifying the type of local time
+    // of the corresponding transition time. These values serve as zero-based
+    // indices into the array of local time type records. The number of type indices
+    // is specified by the "timecnt" field in the header. Each type index be
+    // in the range [0, "typecnt" - 1].
+    var prev_trans_time: i64 = std.math.minInt(i64);
+    for (data.transition_times) |trans| {
+        // Transition time check
+        if (prev_trans_time >= trans.unix_timestamp) return false;
+        prev_trans_time = trans.unix_timestamp;
+
+        // Transion type check
+        if (trans.idx >= header.typecnt) return false;
+    }
+
+    // local time type records
+    // A series of six-octet records specifying a local time type. The number of
+    // records is specified by the "typecnt" field in the header. Each record has
+    // the following format (the lengths of multi-octet fields are shown in parentheses):
+    //
+    // ```
+    //  +---------------+---+---+
+    //  | utoff (4)     |dst|idx|
+    //  +---------------+---+---+
+    // ```
+    //
+    // utoff
+    // A four-octet signed integer specifying the number of seconds to be added
+    // to UT in order to determine local time. The value MUST NOT be -2^31 and
+    // SHOULD be in the range [-89999, 93599] (i.e., its value be more than -25
+    // hours and less than 26 hours).
+    //
+    // Avoiding -2^31 allows 32-bit clients to negate the value without overflow.
+    // Restricting it to [-89999, 93599] allows easy support by implementations
+    // that already support the POSIX- required range [-24:59:59, 25:59:59].
+    //
+    // NOTE: Both the -2^31 and the [-89999, 93599] checks are omited,
+    // warnings are generated while parsing for both
+    //
+    // (is)dst
+    // A one-octet value indicating whether local time should be considered
+    // Daylight Saving Time (DST). The value MUST be 0 or 1. A value of one (1)
+    // indicates that this type of time is DST. A value of zero (0) indicates
+    // that this time type is standard time.
+    //
+    // NOTE: As this is already represented as a bool, this cannot be invalid
+    //
+    // (desig)idx:
+    // A one-octet unsigned integer specifying a zero-based index into the series
+    // of time zone designation octets, thereby selecting a particular designation
+    // string. Each index be in the range [0, "charcnt" - 1]; it designates the
+    // NUL‑terminated string of octets starting at position "idx" in the
+    // time zone designations. (This string MAY be empty.) A NUL octet MUST exist
+    // in the time zone designations at or after position "idx". If the designation
+    // string is "-00", the time type is a placeholder indicating that
+    // local time is unspecified.
+    for (data.local_time_type_records) |rec| {
+        if (rec.designation_idx >= header.charcnt) return false;
+    }
+
+    // time zone designations
+    // A series of octets constituting an array of NUL‑terminated (0x00) time
+    // zone designation strings. The total number of octets is specified by the
+    // "charcnt" field in the header. Two designations overlap if one is a suffix
+    // of the other. The character encoding of time zone designation strings is
+    // not specified; however, see Section 4 of this document.
+    if (data.timezone_designation[data.timezone_designation.len - 1] != 0) return false;
+
+    // leap second records
+    // A series of eight- or twelve-octet records specifying the corrections that
+    // need to be applied to UTC in order to determine TAI, also known as the
+    // leap-second table. The records are sorted by the occurrence time in strictly
+    // ascending order. The number of records is specified by the "leapcnt" field
+    // in the header. Each record has one of the following structures
+    // (the lengths of multi-octet fields are shown in parentheses):
+    //
+    // Version 1 Data Block:
+    // ```
+    // +---------------+---------------+
+    // | occur (4)     | corr (4)      |
+    // +---------------+---------------+
+    // ```
+    //
+    // Version 2+ Data Block:
+    // ```
+    // +---------------+---------------+---------------+
+    // | occur (8)                     | corr (4)      |
+    // +---------------+---------------+---------------+
+    // ```
+    //
+    // occur(ence):
+    // A four- or eight-octet UNIX leap time value specifying the time at which
+    // a leap-second correction occurs or at which the leap-second table expires.
+    // The first value, if present, MUST be non-negative, and each leap second
+    // MUST occur at the end of a UTC month.
+    //
+    // TODO: See if I want to verify end of UTC month, or be more permissive and
+    // extend it to the end of a minute, or allow any second (although how do
+    // we represent that?)
+    //
+    // corr(ection):
+    // A four-octet signed integer specifying the value of LEAPCORR on or after
+    // the occurrence. If "leapcnt" is zero, LEAPCORR is zero for all timestamps.
+    // If "leapcnt" is nonzero, for timestamps before the first occurrence time,
+    // LEAPCORR is zero if the first correction is one (1) or minus one (-1) and
+    // is unspecified otherwise (which can happen only in files truncated at the
+    // start (Section 6.1)).
+    //
+    // The first leap second is a positive leap second if and only if its
+    // correction is positive. Each correction after the first MUST differ from
+    // the previous correction by either one (1) for a positive leap second or
+    // minus one (-1) for a negative leap second, except that in version 4 files
+    // with two or more leap-second records, the correction value of the last two
+    // records MAY be the same, with the occurrence of last record indicating the
+    // expiration time of the leap-second table.
+    //
+    // The leap-second table expiration time is the time at which the table no
+    // longer records the presence or absence of future leap-second corrections,
+    // and post-expiration timestamps cannot be accurately calculated. For example,
+    // a leap-second table published in January, which predicts the presence or
+    // absence of a leap second at June's end, might expire in mid-December because
+    // it is not known when the next leap second will occur.
+    //
+    // If leap seconds become permanently discontinued, as requested by the
+    // General Conference on Weights and Measures, leap-second tables published
+    // after the discontinuation time SHOULD NOT expire, since they will not be
+    // updated in the foreseeable future.
+    //
+    // NOTE: since the expiration date is ?i64, we cannot further verify it
+    var prev_occurence: i64 = -1;
+    var prev_correction: i32 = 0;
+    for (data.leap_second_records) |rec| {
+        if (prev_occurence >= rec.occurrence) return false;
+        prev_occurence = rec.occurrence;
+
+        if (@abs(prev_correction - rec.correction) != 1) return false;
+        prev_correction = rec.correction;
+    }
+
+    // We cannot have an expiration date with < 2 entries
+    if (header.leapcnt < 2 and data.leap_second_expiration != null) return false;
+
+    // standard/wall indicators
+    // A series of one-octet values indicating whether the transition times
+    // associated with local time types were specified as standard time or
+    // wall-clock time. Each value MUST be 0 or 1. A value of one (1) indicates
+    // standard time. The value MUST be set to one (1) if the corresponding UT/local
+    // indicator is set to one (1). A value of zero (0) indicates wall time.
+    // The number of values is specified by the "isstdcnt" field in the header.
+    // If "isstdcnt" is zero (0), all transition times associated with local time
+    // types are assumed to be specified as wall time.
+    //
+    // NOTE: as this is represented as a non exhaustive enum thus cannot be
+    // invalid in that way
+    //
+    // UT/local indicators
+    // A series of one-octet values indicating whether the transition times
+    // associated with local time types were specified as UT or local time.
+    // Each value MUST be 0 or 1. A value of one (1) indicates UT, and the
+    // corresponding standard/wall indicator MUST also be set to one (1). A
+    // value of zero (0) indicates local time. The number of values is specified
+    // by the "isutcnt" field in the header. If "isutcnt" is zero (0), all
+    // transition times associated with local time types are assumed to be
+    // specified as local time.
+    //
+    // NOTE: as this is represented as a non exhaustive enum thus cannot be
+    // invalid in that way
+
+    if (data.std_wall_indicators) |swi| {
+        if (data.ut_local_indicators) |utl| {
+            for (swi, utl) |s, u| {
+                if (u == .universal)
+                    if (s != .standard) return false;
+            }
+        }
+    } else {
+        // All std/wall == .wall
+        if (data.ut_local_indicators) |utl| {
+            for (utl) |u| {
+                if (u == .universal) return false;
+            }
+        }
+    }
+
+    if (header.version == .@"1") return true;
+
+    if (tzif.footer == null) return false;
+
+    // TODO: Add a footer.isValid() check
+
+    return true;
 }
 
 const std = @import("std");
